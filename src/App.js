088468eac6a7b85a999c5c07e1cfb4d63afd44ba
Sean Padden <sean.padden7@gmail.com>
2021-03-15 20:53:14 -0400

    remvoing logs in app

--- src/App.js
+++ src/App.js
@@ -11,21 +11,21 @@ import WordMapContainer from './containers/WordMapContainer.js'
 class App extends Component {
 
 render(){
-return (
-  <div className="App">
-    <Switch>
-      <Route exact path={'/'} render={routerProps => <LoginSignupContainer {...routerProps} />} />
-      <Route path={'/login'} render={routerProps => <LoginSignupContainer {...routerProps} />} />
-      <Route
-        path={'/profile'}
-        render={routerProps => <ProfilePage {...routerProps} />} />
-      <Route path={'/input'} component={WordForm} />
-      <Route path={'/loading'} component={LoadingScreen} />
-      <Route path={'/logout'} component={LogOut} />
-      <Route path={'/map'} component={WordMapContainer} />
-    </Switch>
-  </div>
-)}
+  return (
+    <div className="App">
+      <Switch>
+        <Route exact path={'/'} render={routerProps => <LoginSignupContainer {...routerProps} />} />
+        <Route path={'/login'} render={routerProps => <LoginSignupContainer {...routerProps} />} />
+        <Route
+          path={'/profile'}
+          render={routerProps => <ProfilePage {...routerProps} />} />
+        <Route path={'/input'} component={WordForm} />
+        <Route path={'/loading'} component={LoadingScreen} />
+        <Route path={'/logout'} component={LogOut} />
+        <Route path={'/map'} component={WordMapContainer} />
+      </Switch>
+    </div>
+  )}
 }
 
 export default App;
